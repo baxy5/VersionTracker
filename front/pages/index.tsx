97f8404ac9eb6fbe09a9609f203c96528d52ad1e
baxy5 <bakos.soda@gmail.com>
2022-12-21 11:05:31 +0100

    nav

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,5 +1,6 @@
-import Image from "next/image"
 import { Roboto } from '@next/font/google'
+import Link from "next/link"
+import Image from "next/image"
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -10,8 +11,14 @@ const roboto = Roboto({
 export default function Home() {
   return (
     <div className={roboto.className}>
-      <div className="nav">
-        <p className="text-xl">Version<span className="text-white">Vault</span></p>
+      <div className="flex justify-between items-center p-5">
+        <p className="text-3xl"><span className="text-white">Version</span>Vault</p>
+        <div className='flex'>
+          <Link className='px-2' href="https://github.com/baxy5" target="_blank">
+            Github
+          </Link>
+          <p className='px-2'>Versions</p>
+        </div>
       </div>
       <div className="hero"></div>
       <div className="versiontracker"></div>
