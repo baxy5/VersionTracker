4bea06bca591b2ad83095e9489b9cafbe26b2f99
baxy5 <bakos.soda@gmail.com>
2022-12-23 13:13:38 +0100

    navbar, footer resp

--- front/components/Footer.tsx
+++ front/components/Footer.tsx
@@ -4,11 +4,11 @@ import Link from "next/link"
 const Footer = () => {
     return (
         <div className='flex justify-center items-center p-5'>
-            <div>
-                <Link className='px-2 font-bold' href="https://github.com/baxy5/VersionVault" target="_blank">
+            <div className='text-center'>
+                <Link className='px-2 font-bold' href="https://github.com/baxy5" target="_blank">
                     Github
                 </Link>
-                <p>©️ Bakos Janos</p>
+                <p className='font-bold'>©️ Bakos Janos</p>
             </div>
         </div>
     )
