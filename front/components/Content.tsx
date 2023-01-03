9b2e3f57bce4f947cede4b9f7f2ff568850629ea
baxy5 <bakos.soda@gmail.com>
2023-01-03 12:09:06 +0100

    contentbox style

--- front/components/Content.tsx
+++ front/components/Content.tsx
@@ -2,7 +2,7 @@ import { ContentBox } from "./index"
 
 const Content = ({ data }: any) => {
     return (
-        <div className="md:flex md:justify-evenly pt-10">
+        <div className="flex flex-col gap-5 pt-10 px-3 md:flex-row md:justify-evenly">
             {data.map((item: any) => {
                 return (
                     <ContentBox key={item.id} data={item} />
--- front/components/ContentBox.tsx
+++ front/components/ContentBox.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 const ContentBox = ({ data }: any) => {
     return (
         <div>
-            <div className="w-fit p-3 text-center rounded shadow-lg shadow-[0_0_10px_rgba(147,_64,_255)] ease-in-out transition-all duration-150 hover:shadow-[0_0_20px_rgba(147,_64,_255)]">
+            <div className="md:w-32 lg:w-64 p-3 text-center rounded shadow-lg shadow-[0_0_10px_rgba(147,_64,_255)] ease-in-out transition-all duration-150 hover:shadow-[0_0_20px_rgba(147,_64,_255)]">
                 <h3>{data.name}</h3>
                 <p>{data.version}</p>
                 <Link href={data.link} target="_blank">
